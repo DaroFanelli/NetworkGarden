98eaacc5a68644803860f501871033c136ce82ab
dario.fanelli <dario.fanelli@angras.com.ar>
2023-07-12 17:47:41 -0300

    first commit

---
+++ app/src/main/java/com/esaudev/networkgarden/ui/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.esaudev.networkgarden.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.esaudev.networkgarden.databinding.ActivityMainBinding
+import com.esaudev.networkgarden.ui.nested.MoviesCatalogListAdapter
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private val pokemonListAdapter = PokemonListAdapter()
+    private val moviesCatalogListAdapter = MoviesCatalogListAdapter()
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initRecyclerView()
+        subscribeObservers()
+        viewModel.getMoviesCatalog()
+    }
+
+    private fun initRecyclerView() {
+        binding.moviesCatalog.apply {
+            adapter = moviesCatalogListAdapter
+        }
+
+        moviesCatalogListAdapter.setMovieClickListener {
+            Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun subscribeObservers() {
+        viewModel.catalogList.observe(this) {
+            moviesCatalogListAdapter.submitList(it)
+        }
+    }
+}
---
+++ app/src/main/java/com/esaudev/networkgarden/ui/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.esaudev.networkgarden.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.esaudev.networkgarden.data.local.MoviesProvider
+import com.esaudev.networkgarden.domain.model.MovieCatalog
+import com.esaudev.networkgarden.domain.usecase.GetAllPokemonUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getAllPokemonUseCase: GetAllPokemonUseCase
+): ViewModel() {
+
+    private val _catalogList : MutableLiveData<List<MovieCatalog>> = MutableLiveData()
+    val catalogList : LiveData<List<MovieCatalog>>
+        get() = _catalogList
+
+    fun getMoviesCatalog() {
+        viewModelScope.launch {
+            _catalogList.value = MoviesProvider.movieCatalog
+        }
+    }
+
+}
